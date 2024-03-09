c7a373f803aed43d4076e429ddd95b877f1634e8
Chirag Jain <er.chiragjain92@gmail.com>
2024-03-09 20:32:53 +0000

    day 1 complete

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,10 +1,43 @@
-def main():
+from time import sleep
+
+def day_1():
     result = 0
     with open("input.txt", "r") as fh:
         for line in fh.readlines():
             digits = [int(char) for char in line.strip() if char.isnumeric()]
             result += digits[0] * 10 + (digits[-1] if len(digits) > 1 else digits[0])
         print(result)
+        
+def day_2():
+    result = 0
+    num_map = {
+        "one": 1, 
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    start_chrs = {k[0] for k in num_map.keys()}
+    with open("input.txt", "r") as fh:
+        for line in [l.strip() for l in fh.readlines()]:
+            digits = []
+            for idx in range(len(line)):
+                if line[idx].isnumeric():
+                    digits.append(int(line[idx]))
+                elif line[idx] in start_chrs:
+                    for i in {3, 4, 5}:
+                        if line[idx:idx + i] in num_map:
+                            digits.append(num_map[line[idx:idx + i]])
+            result += digits[0] * 10 + (digits[-1] if len(digits) > 1 else digits[0])
+    print(result)                        
+    
+def main():
+    day_1()
+    day_2()
 
 if __name__ == "__main__":
     main()
