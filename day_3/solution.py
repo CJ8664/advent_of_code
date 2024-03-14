d3963a929cfc526f505dbe146396a2ebb789be9f
Chirag Jain <er.chiragjain92@gmail.com>
2024-03-14 04:12:07 +0000

    day 3 init

---
+++ day_3/solution.py
@@ -0,0 +1,20 @@
+def __get_input_lines():
+    with open("input.txt", "r", encoding="utf-8") as fh:
+        return [line.strip() for line in fh.readlines()]
+
+
+def day_1():
+    lines = __get_input_lines()
+
+
+def day_2():
+    lines = __get_input_lines()
+
+
+def main():
+    day_1()
+    day_2()
+
+
+if __name__ == "__main__":
+    main()
