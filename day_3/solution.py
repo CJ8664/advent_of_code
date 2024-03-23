158bccd5799d2e604b2093729c8b95eb76d89708
Chirag Jain <er.chiragjain92@gmail.com>
2024-03-23 23:12:08 +0000

    day 3 part 1 python

--- day_3/solution.py
+++ day_3/solution.py
@@ -3,8 +3,54 @@ def __get_input_lines():
         return [line.strip() for line in fh.readlines()]
 
 
+sample = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+"""
+
+SPECIAL_CHAR_COORDS = set()
+
+
+def check_candidate(row, cols):
+    for r in [row, row + 1, row - 1]:
+        for c in range(min(cols) - 1, max(cols) + 2):
+            if (r, c) in SPECIAL_CHAR_COORDS:
+                return True
+    return False
+
+
 def day_1():
     lines = __get_input_lines()
+    for row, line in enumerate(lines):
+        for col, ch in enumerate(line):
+            if ch != "." and not ch.isdigit():
+                SPECIAL_CHAR_COORDS.add((row, col))
+
+    result = 0
+    for row, line in enumerate(lines):
+        num = ""
+        cols = set()
+        for col, ch in enumerate(line):
+            if ch.isdigit():
+                num += ch
+                cols.add(col)
+            elif num != "":
+                if check_candidate(row, cols):
+                    result += int(num)
+                num = ""
+                cols = set()
+        if num != "":
+            if check_candidate(row, cols):
+                result += int(num)
+
+    print(result)
 
 
 def day_2():
